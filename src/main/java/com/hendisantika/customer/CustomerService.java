f28e399a4b3f07d17792ce84009fae8094fb4b20
Hendi Santika <hendisantika@gmail.com>
2022-01-22 20:16:49 +0700

    #11 - Add CustomerService

---
+++ src/main/java/com/hendisantika/customer/CustomerService.java
@@ -0,0 +1,28 @@
+package com.hendisantika.customer;
+
+import org.slf4j.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-demo2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/01/22
+ * Time: 20.16
+ */
+@ApplicationScoped
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+    private final Logger logger;
+
+    public CustomerService(CustomerRepository customerRepository, CustomerMapper customerMapper, Logger logger) {
+        this.customerRepository = customerRepository;
+        this.customerMapper = customerMapper;
+        this.logger = logger;
+    }
+}

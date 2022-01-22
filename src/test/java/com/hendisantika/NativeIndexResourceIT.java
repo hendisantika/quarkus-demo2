c8bc6ed42f7fc29e3b3e55fcc4194c3251af2984
Hendi Santika <hendisantika@gmail.com>
2022-01-22 20:00:43 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/hendisantika/NativeIndexResourceIT.java
@@ -0,0 +1,9 @@
+package com.hendisantika;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeIndexResourceIT extends com.hendisantika.IndexResourceTest {
+
+    // Execute the same tests but in native mode.
+}

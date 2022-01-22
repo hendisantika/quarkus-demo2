879bf9aa5f1ed6a861c873c1ed7907e38ba0e826
Hendi Santika <hendisantika@gmail.com>
2022-01-22 20:14:41 +0700

    #7 - Add Customer

---
+++ src/main/java/com/hendisantika/customer/Customer.java
@@ -0,0 +1,116 @@
+package com.hendisantika.customer;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import java.util.Objects;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-demo2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/01/22
+ * Time: 20.14
+ */
+public class Customer {
+    private Integer customerId;
+
+    @NotEmpty
+    private String firstName;
+
+    private String middleName;
+
+    @NotEmpty
+    private String lastName;
+
+    private String suffix;
+
+    @Email
+    private String email;
+
+    private String phone;
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(customerId, customer.customerId) && Objects.equals(firstName, customer.firstName) && Objects.equals(middleName, customer.middleName) && Objects.equals(lastName, customer.lastName) && Objects.equals(suffix, customer.suffix) && Objects.equals(email, customer.email) && Objects.equals(phone, customer.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customerId, firstName, middleName, lastName, suffix, email, phone);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", suffix='" + suffix + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+}
